132fa9c5c2e76adb07fcc104f70c3f9f1ab0df1f
jmoeil <juian.moeil@ulb.be>
2025-03-12 16:08:03 +0100

    Fixed fitting issues

--- macros/fit/fitting.py
+++ macros/fit/fitting.py
@@ -20,35 +20,37 @@ with open(output_filename, 'w') as output_file:
 			exit()
 
 		# Define the variable
-		x = ROOT.RooRealVar("mjj", "mjj", 0, 1000)
-		x.setRange("fitRange", 60, 350)
+		x = ROOT.RooRealVar("mjj", "mjj", 0, 200)
+		x.setRange("fitRange", 40, 160)
 		#dh = ROOT.RooDataHist("dh", "dh", [x], Import=hh)
 
 		# Define the signal (gaussian)
-		mean = ROOT.RooRealVar("mean", "mean", 91.2, 85, 110)
+		mean = ROOT.RooRealVar("mean", "mean", 90, 85, 110)
 		sigma = ROOT.RooRealVar("sigma", "sigma", 10, 5, 25) 
 		gx = ROOT.RooGaussian("gauss", "gauss", x, mean, sigma)
 
 
 		# Define the background (Chebyshev polynomial)
-		a = 4
+		
+		a = 10000
 		a0 = ROOT.RooRealVar("a0", "a0", 0, -a, a)
 		a1 = ROOT.RooRealVar("a1", "a1", 0, -a, a)
-		a2 = ROOT.RooRealVar("a2", "a2", 0, -a, a)
-		a3 = ROOT.RooRealVar("a3", "a3", 0, -a, a)
-		px = ROOT.RooChebychev("px", "px", x, ROOT.RooArgList(a0, a1, a2, a3))
-
+		#a2 = ROOT.RooRealVar("a2", "a2", 0, -a, a)
+		#a3 = ROOT.RooRealVar("a3", "a3", 0, -a, a)
+		#px = ROOT.RooChebychev("px", "px", x, ROOT.RooArgList(a0, a1, a2, a3))
+		px = ROOT.RooChebychev("px", "px", x, ROOT.RooArgList(a0, a1))
+		
 		# Just trying another background (exponential)
 
-		#p0 = ROOT.RooRealVar('p0', 'p0', -0.01, -1, 0)
-		#p2 = ROOT.RooExponential('p2', 'p2', x, p0)
-
-
+		p0 = ROOT.RooRealVar('p0', 'p0', -0.01, -1, 0)
+		p2 = ROOT.RooExponential('p2', 'p2', x, p0)
+		
 		# Construct composite model : signal + background 
 		f = ROOT.RooRealVar("f", "f", 0.5, 0.0, 1.0)
 		model = ROOT.RooAddPdf("model", "model", ROOT.RooArgList(gx, px), ROOT.RooArgList(f))
 		#model = ROOT.RooAddPdf('model', 'model', ROOT.RooArgList(gx, p2), ROOT.RooArgList(f))
-
+		#model = ROOT.RooAddPdf("model", "model", ROOT.RooArgList(gx, bkg), ROOT.RooArgList(f))
+		
 		# Convert histogram to RooDataHist
 		dh = ROOT.RooDataHist(f"dh_{flavour}", f"dataset from histogram {hist_name}", ROOT.RooArgList(x), hh)
 
@@ -58,13 +60,16 @@ with open(output_filename, 'w') as output_file:
 		#hh.Scale(1/hh.Integral()) # To normalize the data
 		fitResult = model.fitTo(dh, ROOT.RooFit.Range('fitRange'), PrintLevel=-1, Save=True)
 		fitResult.Print()
-
+		
 		# Plot model on data
 
 		frame = x.frame(Title=f"Fit for Flavour {flavour}")
 		dh.plotOn(frame)
-		model.plotOn(frame, Components='gx', LineStyle=2, LineColor=ROOT.kRed)
-		model.plotOn(frame, Components="px", LineStyle=2, LineColor=ROOT.kBlue)
+		model.plotOn(frame, Components="gauss", LineStyle=2, LineColor=ROOT.kRed, Name='Signal')
+		model.plotOn(frame, Components="px", LineStyle=2, LineColor=ROOT.kBlue, Name='Background')
+		model.plotOn(frame,LineStyle=2,LineColor=ROOT.kMagenta, Name='TotalFit')
+
+		chi2 = frame.chiSquare()
 
 		# Add legend
 		leg = ROOT.TLegend(0.6, 0.7, 0.88, 0.88)
@@ -72,15 +77,17 @@ with open(output_filename, 'w') as output_file:
 		leg.SetTextSize(0.03)  # Edits text length
 		leg.SetFillStyle(0)  # Transparent background
 
-		leg.AddEntry(frame.findObject("mij"), "Total Fit", "l")
-		leg.AddEntry(frame.findObject("gx_Norm[mjj]"), "Signal (Z)", "l")
-		leg.AddEntry(frame.findObject("px_Norm[mjj]"), "Bruit de fond (Chebyshev)", "l")
+		leg.AddEntry(frame.findObject("TotalFit"), "Total Fit", "l")
+		leg.AddEntry(frame.findObject("Signal"), "Signal (Z)", "l")
+		leg.AddEntry(frame.findObject("Background"), "Background (Chebyshev)", "l")
+		#leg.AddEntry('', f'Chi^{{2}}/ndof = {chi2:.2f}','')
 
 		c = ROOT.TCanvas(f"c_{flavour}", f"Fit for Flavour {flavour}", 800, 400)
 		ROOT.gPad.SetLeftMargin(0.15)
 		frame.GetYaxis().SetTitleOffset(1.4)
 		frame.GetYaxis().SetTitleOffset(1.4)
 		frame.Draw()
+		leg.Draw()
 		c.SaveAs(f"fit_{flavour}.png")
 
 		resid = frame.pullHist()  # Get pull histogram
@@ -91,15 +98,14 @@ with open(output_filename, 'w') as output_file:
 		pull_frame.Draw()
 		c2.SaveAs(f"pull_distribution_flavour{flavour}.png")
 
-		chi2 = frame.chiSquare()
 		
 		# Save fit results in the text file
 		output_file.write(f"\nFlavour {flavour} Results:\n")
 		output_file.write(f"Chi-Squared/Ndof: {chi2:.3f}\n")
 		output_file.write(f"a0 = {a0.getVal():.3f} ± {a0.getError():.3f}\n")
 		output_file.write(f"a1 = {a1.getVal():.3f} ± {a1.getError():.3f}\n")
-		output_file.write(f"a2 = {a2.getVal():.3f} ± {a2.getError():.3f}\n")
-		output_file.write(f"a3 = {a3.getVal():.3f} ± {a3.getError():.3f}\n")
+		#output_file.write(f"a2 = {a2.getVal():.3f} ± {a2.getError():.3f}\n")
+		#output_file.write(f"a3 = {a3.getVal():.3f} ± {a3.getError():.3f}\n")
 		output_file.write(f"f  = {f.getVal():.3f} ± {f.getError():.3f}\n")
 		output_file.write(f"mean = {mean.getVal():.3f} ± {mean.getError():.3f}\n")
 		output_file.write(f"sigma = {sigma.getVal():.3f} ± {sigma.getError():.3f}\n")
